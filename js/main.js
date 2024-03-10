3ce1ddb050876fe56feb023d47627e460f733944
bdtd <162292428+bdtd@users.noreply.github.com>
2024-03-10 23:24:35 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -468,6 +468,32 @@
         clBackToTop();
 
     })();
+    let slideIndex = 0;
+    showSlides();
+    
+    function showSlides() {
+      const slides = document.querySelectorAll('.slide');
+      const dots = document.querySelectorAll('.dot');
+    
+      for (const slide of slides) {
+        slide.style.display = 'none';
+      }
+    
+      slideIndex++;
+    
+      if (slideIndex > slides.length) {
+        slideIndex = 1;
+      }
+    
+      for (const dot of dots) {
+        dot.classList.remove('active');
+      }
+    
+      slides[slideIndex - 1].style.display = 'block';
+      dots[slideIndex - 1].classList.add('active');
+    
+      setTimeout(showSlides, 3000); // Change slide every 3 seconds (adjust as needed)
+    }
 
 
 })(jQuery);
